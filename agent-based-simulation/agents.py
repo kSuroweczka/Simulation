472b0b2ff4e4292a83b3a449abdefcfb0454d84c
Milena Smołka <milena.m.smolka@gmail.com>
2023-12-21 12:33:58 +0100

    better performance

--- agent-based-simulation/agents.py
+++ agent-based-simulation/agents.py
@@ -1,6 +1,8 @@
 import mesa
+import heapq
 import random
 from utils import State
+import numpy as np  
 
 global CELLS_OCCUPIED_BY_STUDENTS
 CELLS_OCCUPIED_BY_STUDENTS =[]
@@ -42,101 +44,118 @@ class StudentAgent(mesa.Agent):
         self.target_exit: (int,int)
         self.path_to_exit: list[(int,int)] = None
 
-        CELLS_OCCUPIED_BY_STUDENTS.append(position) 
-
-    def find_target_exit(self): 
-        path =[]
-        cost =[]
+        CELLS_OCCUPIED_BY_STUDENTS.append(position)
+    
+    def calculate_nearest_exit(self, position, exit):
+        return np.linalg.norm(np.array(position) - np.array(exit))
         
-        for exit in self.exits:
-            path_i,cost_i = self.aStarSearch(self.current_position,exit)
-            path.append(path_i)
-            cost.append(cost_i)
+    def find_target_exit(self):
+        self.target_exit = min(self.exits, key=lambda exit: self.calculate_nearest_exit(self.current_position, exit))
+        self.path_to_exit, _ = self.aStarSearch(self.current_position, self.target_exit)
+        
+        
+
+    # def find_target_exit(self): 
+    #     path =[]
+    #     cost =[]
+        
+    #     for exit in self.exits:
+    #         path_i,cost_i = self.aStarSearch(self.current_position,exit)
+    #         path.append(path_i)
+    #         cost.append(cost_i)
     
-        min_cost = min(cost)
-        min_cost_index = cost.index(min_cost)
+    #     min_cost = min(cost)
+    #     min_cost_index = cost.index(min_cost)
 
-        self.path_to_exit = path[min_cost_index] 
-        self.target_exit = self.path_to_exit[-1]
+    #     self.path_to_exit = path[min_cost_index] 
+    #     self.target_exit = self.path_to_exit[-1]
     
-    def aStarSearch(self, start, stop) -> (list[(int,int)],int): 
-        open_lst = set([start]) 
-        closed_lst = set([])
 
-        g = {}
-        g[start] = 0
+    def aStarSearch(self, start, stop):
+        open_lst = [(0, start)]  
+        heapq.heapify(open_lst)  
 
-        par = {}   ### parent
-        par[start] = start
- 
-        while len(open_lst) > 0:
-            n = None
+        closed_lst = set()
 
-            for v in open_lst:
-                if n == None or g[v] + self.H(v,stop) < g[n] + self.H(n, stop):
-                    n = v
- 
-            if n == None:
-                print('Path does not exist!')
-                return None
+        g = {start: 0}
+
+        par = {start: start}
+
+        while open_lst:
+            _, n = heapq.heappop(open_lst)
 
             if n == stop:
-                reconst_path = []
-                while par[n] != n:
-                    reconst_path.append(n)
-                    n = par[n]
-                reconst_path.append(start)
-                reconst_path.reverse()
-                return reconst_path, len(reconst_path)
-            
-            x=n[0]
-            y=n[1]
-            neighbors = [ (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y),(x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
-            for m in neighbors:
-                if m not in open_lst and m not in closed_lst and m not in self.obstacles:
-                    open_lst.add(m)
-                    par[m] = n
-                    g[m] = g[n] + 10
+                return self.reconstruct_path(par, n), g[n]
 
-                else:
-                    if m not in self.obstacles and g[m] > g[n] + 10: # na tym się chyba coś zacina
-                        g[m] = g[n] + 10
+            x, y = n
+            neighbors = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y),
+                         (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+
+            for m in neighbors:
+                if m not in closed_lst and m not in self.obstacles:
+                    tentative_g_score = g[n] + 10
+
+                    if m not in g or tentative_g_score < g[m]:
+                        g[m] = tentative_g_score
+                        f_score = tentative_g_score + self.H(m, stop)
+                        heapq.heappush(open_lst, (f_score, m))
                         par[m] = n
- 
-                        if m in closed_lst:
-                            closed_lst.remove(m)
-                            open_lst.add(m)
-            open_lst.remove(n)
+
             closed_lst.add(n)
- 
+
         print('Path does not exist!')
         return None
 
+    def reconstruct_path(self, par, current):
+        path = []
+        while par[current] != current:
+            path.append(current)
+            current = par[current]
+        path.append(current)
+        return path[::-1]
+    
     def move(self):
-
         if len(self.path_to_exit) == 0:
-            print("CEL OSIAGNIETY")
-            # self.model.grid.remove_agent(self)
+            print("Goal reached")
             self.state = State.ESCAPED
-            self.currrent_position = self.current_position
-        else:
-            new_position = self.path_to_exit[0] 
 
-            # is_not_occupied = self.model.grid.get_cell_list_contents([new_position]) == 0
-            if new_position not in self.obstacles :
+        else:
+            nearest_exit = min(self.exits, key=lambda exit: self.calculate_nearest_exit(self.current_position, exit))
+            if nearest_exit != self.target_exit:
+                self.target_exit = nearest_exit
+                self.path_to_exit, _ = self.aStarSearch(self.current_position, self.target_exit)
+           
+            new_position = self.path_to_exit[0]
+            if new_position not in self.obstacles:
                 self.model.grid.move_agent(self, new_position)
                 CELLS_OCCUPIED_BY_STUDENTS.remove(self.current_position)
-                if self.current_position != self.path_to_exit[-1]: 
-                    self.path_to_exit.remove(self.path_to_exit[0])
+                if self.current_position != self.path_to_exit[-1]:
+                    self.path_to_exit.pop(0)
                     CELLS_OCCUPIED_BY_STUDENTS.append(new_position)
-                self.current_position = new_position  
-            else:
-                self.currrent_position = self.current_position
-
+                self.current_position = new_position
 
                     
-    def H(self, position,  exit):
-        return 10 *(abs(position[0] - exit[0]) + abs(position[1] - exit[1]))
+                
+                    
+    def H(self, position, exit):
+        position = np.array(position)
+        exit = np.array(exit)
+        return 10 * np.sum(np.abs(position - exit))
     
     def step(self):
         self.move()
+        
+    # def random_move(self):
+    #     possible_moves = self.model.grid.get_neighborhood(self.current_position, moore=True, include_center=False)
+    #     new_position = random.choice(possible_moves)
+    #     while new_position in self.obstacles:
+    #         new_position = random.choice(possible_moves)
+    #     self.model.grid.move_agent(self, new_position)
+    #     CELLS_OCCUPIED_BY_STUDENTS.remove(self.current_position)
+    #     CELLS_OCCUPIED_BY_STUDENTS.append(new_position)
+    #     self.current_position = new_position
+    #     self.path_to_exit, _ = self.aStarSearch(self.current_position, self.target_exit)
+    #     self.target_exit = self.path_to_exit[-1]
+        
+    
+        
